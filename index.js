9e80d4f5a7c3e61ad92df192a1a3437a238f3484
Luis <luisrc91@live.com>
2024-07-19 15:03:22 -0400

    added javascript to add messages

---
+++ index.js
@@ -0,0 +1,32 @@
+const $ = (el) => document.querySelector(el);
+
+const $form = $("form");
+const $input = $("input");
+const $template = $("#message-template");
+const $message = $("ul");
+const $container = $("main");
+const $button = $("button");
+
+$form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const messageText = $input.value.trim();
+
+  if (messageText !== "") {
+    $input.value = "";
+    addMessage(messageText, 'user');
+  }
+});
+
+function addMessage(text, sender) {
+  const clonedTemplate = $template.content.cloneNode(true);
+  const $newMessage = clonedTemplate.querySelector(".message");
+
+  const $who = $newMessage.querySelector("span");
+  const $text = $newMessage.querySelector("p");
+
+  $text.textContent = text;
+  $who.textContent = sender === "bot" ? "GPT" : "Me";
+  $newMessage.classList.add(sender);
+
+  $message.appendChild($newMessage);
+}
